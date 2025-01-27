2f7fda9ba64e602891d96353ed1f3b8792a556cb
farabi23 <78350272+farabi23@users.noreply.github.com>
2025-01-28 04:32:41 +0500

    cotroller, service and body

---
+++ src/main/java/com/email/writer/EmailGeneratorService.java
@@ -0,0 +1,15 @@
+package com.email.writer;
+
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class EmailGeneratorService {
+
+    public String generateEmailReply(EmailRequest emailRequest) {
+
+
+
+    }
+
+}

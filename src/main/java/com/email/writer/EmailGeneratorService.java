e3fb1c81b6e3fa4bf655e9bea15cf68f9d29b3a0
farabi23 <78350272+farabi23@users.noreply.github.com>
2025-01-28 19:32:18 +0500

    prompt, craft request, adding keys

--- src/main/java/com/email/writer/EmailGeneratorService.java
+++ src/main/java/com/email/writer/EmailGeneratorService.java
@@ -1,15 +1,47 @@
 package com.email.writer;
 
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
+
+import java.util.Map;
+import java.util.Objects;
 
 @Service
 public class EmailGeneratorService {
 
+    @Value("${gemini.api.url}")
+    private String geminiApiUrl;
+    @Value("${gemini.api.key}")
+    private String geminiApiKey;
+
     public String generateEmailReply(EmailRequest emailRequest) {
+        // the prompt
+        String prompt = buildPrompt(emailRequest);
+        // craft a request
+        Map<String, Object> requestBody = Map.of(
+                "contents", new Object[]{
+                        Map.of("parts", new Object[]{
+                                Map.of("text", prompt)
+                        })
+                }
 
+        );
+        // do request and get response
+        // return response
 
+    }
 
+    private String buildPrompt(EmailRequest emailRequest) {
+        StringBuilder prompt = new StringBuilder();
+        prompt.append("Generate a professional email reply the following email content" +
+                ". Please don't generate a subject line ");
+        if(emailRequest.getTone() != null && !emailRequest.getTone().isEmpty()) {
+            prompt.append("Use a ").append(emailRequest.getTone()).append(" tone.");
+
+        }
+        prompt.append("\nOriginal email: \n").append(emailRequest.getEmailContent());
+        return prompt.toString();
     }
 
 }
--- src/main/java/com/email/writer/EmailRequest.java
+++ src/main/java/com/email/writer/EmailRequest.java
@@ -8,4 +8,29 @@ public class EmailRequest {
     private String emailContent;
 
     private String tone;
+
+    public EmailRequest(String emailContent, String tone) {
+        this.emailContent = emailContent;
+        this.tone = tone;
+
+    }
+    public EmailRequest() {
+
+    }
+
+    public String getEmailContent() {
+        return emailContent;
+    }
+
+    public void setEmailContent(String emailContent) {
+        this.emailContent = emailContent;
+    }
+
+    public String getTone() {
+        return tone;
+    }
+
+    public void setTone(String tone) {
+        this.tone = tone;
+    }
 }

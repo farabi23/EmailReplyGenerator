07258bc5b2133018d2000bcafe0d8a5268f7f220
farabi23 <78350272+farabi23@users.noreply.github.com>
2025-02-02 15:05:31 +0500

    finalize the front, api call, containers

--- emailReact-front/src/App.jsx
+++ emailReact-front/src/App.jsx
@@ -1,6 +1,7 @@
 
 import { useState } from 'react';
-import { Container, TextField, Typography, Box, FormControl, InputLabel, MenuItem } from '@mui/material';
+import { Container, TextField, Typography, Box, FormControl, 
+  InputLabel, MenuItem, Select, CircularProgress, Button} from '@mui/material';
 import './App.css'
 
 function App() {
@@ -12,6 +13,24 @@ function App() {
   const [loading, setLoading] = useState(false);
 
   const[error, setError] = useState('');
+
+  const handleSubmit = async () =>{
+    setLoading(true);
+    setError('');
+    try{
+        const respone = await axios.post("http://localhost:8080/api/email/generate",
+          {emailContent, tone});
+          setGeneratedReply(typeof respone.data==="string" ? respone.data : JSON.stringify(respone.data));
+    }
+    catch(error){
+      setError('Failed to generate email reply. Please try again');
+      console.error(error);
+    } finally {
+      setLoading(false);
+    }
+
+  };
+
 
   return (
     <Container maxWidth="md" sx={{py:4}}>
@@ -26,7 +45,7 @@ function App() {
          sx={{mb:2}}
          />
 
-         <FormControl>
+         <FormControl fullWidth sx={{mb:2}}>
           <InputLabel>Tone (Optional)</InputLabel>
           <Select 
           value={tone || ''}
@@ -40,8 +59,44 @@ function App() {
         
           </Select>
          </FormControl>
+         <Button
+         variant='contained'
+         onClick={handleSubmit}
+         disabled={!emailContent || loading}
+         fullWidth>
+          {loading ? <CircularProgress size={24}/> : "Generate Reply"}
+         </Button>
 
       </Box>
+
+      {error && (
+        <Typography  color='error' sx={{mb:2}}>
+        {error}
+      </Typography>
+
+      ) }
+
+      {generatedReply && (
+        <Box sx={{mt:3}}>
+          <Typography variant='h6' gutterBottom>
+            Generated Reply:
+          </Typography>
+          <TextField
+          fullWidth
+          multiline
+          rows={6}
+          variant='outlined'
+          value={generatedReply || ''}
+          inputProps={{ readOnly: true }}/>
+
+          <Button
+          variant='outlined' sx={{mt:2}}
+          onClick={() => navigator.clipboard.writeText(generatedReply)}>
+            Copy to Clipboard
+          </Button>
+        </Box>
+      )
+      }
 
     </Container>
   )
